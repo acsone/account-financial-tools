c9934716e380c62145c585830dcb8b84bd855e48
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-06-30 11:12:36 +0200

    Set all addons to uninstallable

--- account_chart_update/__openerp__.py
+++ account_chart_update/__openerp__.py
@@ -70,5 +70,5 @@ Any problem found while updating will be shown on the last step.
         'wizard/wizard_chart_update_view.xml',
     ],
     "active": False,
-    "installable": True
+    'installable': False
 }
