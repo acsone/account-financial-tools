310059498756b9a94a3391ca3c5007835c9a9653
Adrien Peiffer <adrien.peiffer@acsone.eu>
2014-09-03 15:07:31 +0200

    [IMP] Improve behavior that add some custom fields on POS customer view without overwriting

--- account_credit_limit_pos/static/src/js/account_credit_limit_pos_model.js
+++ account_credit_limit_pos/static/src/js/account_credit_limit_pos_model.js
@@ -5,14 +5,18 @@ function account_credit_limit_pos_models(instance, module){ //module is instance
     var round_di = instance.web.round_decimals;
     var round_pr = instance.web.round_precision;
     
-    /*module.PosModel.include({
-        load_server_data: function(){
-            this._super();
-            var self = this;
-            var loaded = self.fetch('res.partner', ['name','street','city','country_id','phone','zip','mobile','email','ean13','blocked_customer']).then(function(partners){
-                self.partners = partners;
-                self.db.add_partners(partners);
-            });
+    var _super_initialize = module.PosModel.prototype.initialize;
+    
+    function AddCustomerFields(pos_model) {
+        var res_partner_model = pos_model.find_model('res.partner');
+        if (Object.size(res_partner_model) == 1) {
+            var res_partner_index = parseInt(Object.keys(res_partner_model)[0]);
+            pos_model.models[res_partner_index].fields.push('property_payment_term','blocked_customer','amount_blocked')
         }
-    });*/
+    };
+    
+    module.PosModel.prototype.initialize = function(session, attributes){
+        _super_initialize.call(this, session, attributes);
+        AddCustomerFields(this);
+    }   
 }
