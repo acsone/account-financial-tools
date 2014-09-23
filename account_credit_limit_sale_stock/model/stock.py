cad7a5a28d41b4434d927e222217b57273a8be6e
Adrien Peiffer <adrien.peiffer@acsone.eu>
2014-09-23 16:17:02 +0200

    [IMP] Improve stock picking forcing

--- account_credit_limit_sale_stock/model/stock.py
+++ account_credit_limit_sale_stock/model/stock.py
@@ -67,62 +67,66 @@ class stock_picking(orm.Model):
             else:
                 return res
         else:
-            return super(stock_picking, self).do_transfer(cr,
-                                                          uid,
-                                                          ids,
-                                                          context=context)
+            res = super(stock_picking, self).do_transfer(cr,
+                                                         uid,
+                                                         ids,
+                                                         context=context)
+            stock_picking_obj = self.browse(cr, uid, ids, context=context)[0]
+            stock_move_model = self.pool.get('stock.move')
+            stock_move_ids = stock_move_model\
+                .search(cr,
+                        uid,
+                        [('picking_id.id', '=', stock_picking_obj.id),
+                         ('procurement_id', '<>', False)])
+            amount_order = 0.0
+            for stock_move in stock_move_model.browse(cr,
+                                                      uid,
+                                                      stock_move_ids,
+                                                      context=context):
+                sale_order_line = stock_move.procurement_id.sale_line_id
+                tax_ids = sale_order_line.tax_id
+                tax = 1.0
+                for tax_id in tax_ids:
+                    tax = tax + tax_id.amount
+                amount_order = amount_order +\
+                    ((sale_order_line.price_unit *
+                      stock_move.product_uom_qty) * tax)
+            if stock_picking_obj.partner_id.commercial_partner_id.level_amount\
+                    is None:
+                amount = _('Manual Blocking')
+            else:
+                amount = amount_order
+            diff = stock_picking_obj.partner_id\
+                .commercial_partner_id.credit_limit - \
+                stock_picking_obj.partner_id.commercial_partner_id.level_amount
+            if (diff > 0):
+                amount = amount - diff
+            self.pool.get('force.tracking')\
+                .create(cr,
+                        uid,
+                        {'user_id': uid,
+                         'type': 'delivery_order',
+                         'source_document': stock_picking_obj.name,
+                         'partner_id': stock_picking_obj.partner_id
+                            .commercial_partner_id.id,
+                         'amount': str(amount),
+                         })
+            return res
 
 
 class warning_force_picking_wizard(orm.TransientModel):
     _name = 'warning.force.picking.wizard'
 
     def validate(self, cr, uid, ids, context=None):
-        stock_picking_id = context.get('stock_picking_id')
-        stock_picking = self.pool.get('stock.picking')\
-            .browse(cr, uid, [stock_picking_id], context=context)[0]
-        stock_move_model = self.pool.get('stock.move')
-        stock_move_ids = stock_move_model\
-            .search(cr,
-                    uid,
-                    [('picking_id.id', '=', stock_picking.id),
-                     ('procurement_id', '<>', False)])
-        amount_order = 0.0
-        for stock_move in stock_move_model.browse(cr,
-                                                  uid,
-                                                  stock_move_ids,
-                                                  context=context):
-            sale_order_line = stock_move.procurement_id.sale_line_id
-            tax_ids = sale_order_line.tax_id
-            tax = 1.0
-            for tax_id in tax_ids:
-                tax = tax + tax_id.amount
-            amount_order = amount_order + sale_order_line.price_subtotal * tax
-
-        if stock_picking.partner_id.commercial_partner_id.level_amount \
-                is None:
-            amount = _('Manual Blocking')
-        else:
-            amount = amount_order
-        diff = stock_picking.partner_id\
-            .commercial_partner_id.credit_limit - \
-            stock_picking.partner_id.commercial_partner_id.level_amount
-        if (diff > 0):
-            amount = amount - diff
-        self.pool.get('force.tracking')\
-            .create(cr,
-                    uid,
-                    {'user_id': uid,
-                     'type': 'delivery_order',
-                     'source_document': stock_picking.name,
-                     'partner_id': stock_picking.partner_id
-                        .commercial_partner_id.id,
-                     'amount': str(amount),
-                     })
-        return self.pool.get('stock.picking')\
-            .do_transfer(cr,
-                         uid,
-                         [stock_picking.id],
-                         context=context)
+        if context is None:
+            return False
+        context.update({'force': True})
+        transfer_detail_id = context.get('transfer_detail_id')
+        return self.pool.get('stock.transfer_details')\
+            .do_detailed_transfer(cr,
+                                  uid,
+                                  transfer_detail_id,
+                                  context=context)
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
