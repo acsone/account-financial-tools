b0187bb70cf7a195e9529d9cb220a571067e7db5
Laetitia Gangloff <laetitia.gangloff@acsone.eu>
2015-02-04 18:07:22 +0100

    account_move_validation_improvement : add exclude account.move

--- account_move_validation_improvement/wizard/account_validate_move.py
+++ account_move_validation_improvement/wizard/account_validate_move.py
@@ -32,12 +32,35 @@ class ValidateAccountMove(orm.TransientModel):
         'period_ids': fields.many2many('account.period', string='Periods',
                                        required=True,
                                        domain=[('state', '<>', 'done')]),
+        'exclude_move_ids': fields.many2many('account.move',
+                                             string='Exclude Journal Entries'),
         # re-define existing fields as non-mandatory
         'journal_id': fields.many2one('account.journal', 'Journal',
                                       required=False),
         'period_id': fields.many2one('account.period', 'Period',
                                      required=False),
     }
+
+    def _get_domain(self, cr, uid, journal_ids, period_ids, exclude_move_ids):
+        return [('state', '=', 'draft'),
+                ('journal_id', 'in', journal_ids),
+                ('period_id', 'in', period_ids),
+                ('id', 'not in', exclude_move_ids),
+                ]
+
+    def on_change_journal_period(self, cr, uid, ids, journal_ids, period_ids,
+                                 exclude_move_ids):
+        res = {}
+        journal_ids = journal_ids[0][2]
+        period_ids = period_ids[0][2]
+        exclude_move_ids = exclude_move_ids[0][2]
+        domain = [('id', '=', -1)]
+        if journal_ids or period_ids:
+            domain = self._get_domain(cr, uid, journal_ids,
+                                      period_ids, exclude_move_ids)
+
+        res['domain'] = {'exclude_move_ids': str(domain)}
+        return res
 
     def validate_move(self, cr, uid, ids, context=None):
         obj_move = self.pool.get('account.move')
@@ -46,9 +69,12 @@ class ValidateAccountMove(orm.TransientModel):
         data = self.browse(cr, uid, ids, context=context)[0]
         journal_ids = [journal.id for journal in data.journal_ids]
         period_ids = [period.id for period in data.period_ids]
-        ids_move = obj_move.search(cr, uid, [('state', '=', 'draft'),
-                                             ('journal_id', 'in', journal_ids),
-                                             ('period_id', '=', period_ids)],
+        exclude_move_ids = [move.id for move in data.exclude_move_ids]
+        ids_move = obj_move.search(cr, uid,
+                                   self._get_domain(cr, uid,
+                                                    journal_ids,
+                                                    period_ids,
+                                                    exclude_move_ids),
                                    order='date',
                                    context=context)
         if not ids_move:
