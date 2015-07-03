6f739adf62b2d15a9121c8798b5ba3b1c46be4f1
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-07-03 14:37:30 +0200

    [ADD] Add account_careof_credit_control module

---
+++ account_careof_credit_control/wizard/credit_control_communication.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+##############################################################################
+#
+#    Authors: Adrien Peiffer
+#    Copyright (c) 2015 Acsone SA/NV (http://www.acsone.eu)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, api, fields
+
+
+class CreditControlCommunication(models.TransientModel):
+    _inherit = 'credit.control.communication'
+
+    @api.depends('partner_id')
+    @api.one
+    def _compute_account_careof_partner_id(self):
+        self.account_careof_partner_id = self.partner_id\
+            .get_account_careof_partner(self.report_date)
+
+    account_careof_partner_id = fields.Many2one(
+        compute='_compute_account_careof_partner_id',
+        comodel_name='res.partner', string='Care Of')
+
+    @api.multi
+    def get_email(self):
+        if not self.account_careof_partner_id.id:
+            email = super(CreditControlCommunication, self).get_email()
+        else:
+            email = self.account_careof_partner_id.email
+        return email
