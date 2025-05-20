0352add17de0c02c7f9feda08bf04f4fc305290f
Anusha <anusha.pp@calin.co.in>
2025-05-20 14:29:10 +0200

    [MIG] account_invoice_constraint_chronology: Migration to 18.0

--- account_invoice_constraint_chronology/tests/test_account_invoice_constraint_chronology.py
+++ account_invoice_constraint_chronology/tests/test_account_invoice_constraint_chronology.py
@@ -8,24 +8,24 @@ from freezegun import freeze_time
 
 from odoo import fields
 from odoo.exceptions import UserError
-from odoo.tests import common
+from odoo.tests import Form
+from odoo.tests.common import TransactionCase
 
 
 # in order to avoid a raise caused by the sequence mixin we must ensure
 # that all dates are in the same year, hence we freeze the date
 # in the middle of the year
 @freeze_time(f"{date.today().year}-07-01")
-class TestAccountInvoiceConstraintChronology(common.TransactionCase):
+class TestAccountInvoiceConstraintChronology(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
         cls.company = cls.env.ref("base.main_company")
         cls.partner_2 = cls.env.ref("base.res_partner_2")
+        cls.partner_12 = cls.env.ref("base.res_partner_12")
         cls.today = fields.Date.today()
         cls.yesterday = cls.today - timedelta(days=1)
         cls.tomorrow = cls.today + timedelta(days=1)
-
         cls.AccountJournal = cls.env["account.journal"]
         cls.sale_journal = cls.AccountJournal.create(
             {
@@ -37,10 +37,10 @@ class TestAccountInvoiceConstraintChronology(common.TransactionCase):
         )
 
         cls.ProductProduct = cls.env["product.product"]
+        cls.AccountChartTemplate = cls.env["account.chart.template"]
         cls.product = cls.ProductProduct.create({"name": "Product"})
-
         cls.AccountMove = cls.env["account.move"]
-        with common.Form(
+        with Form(
             cls.AccountMove.with_context(default_move_type="out_invoice")
         ) as invoice_form:
             invoice_form.invoice_date = cls.today
@@ -50,21 +50,20 @@ class TestAccountInvoiceConstraintChronology(common.TransactionCase):
             cls.invoice_1 = invoice_form.save()
         cls.invoice_1.update({"journal_id": cls.sale_journal})
         cls.invoice_2 = cls.invoice_1.copy()
-
         cls.AccountMoveReversal = cls.env["account.move.reversal"]
 
     def test_journal_type_change(self):
         self.assertTrue(self.sale_journal.check_chronology)
 
-        with common.Form(self.sale_journal) as form:
+        with Form(self.sale_journal) as form:
             form.type = "general"
         self.assertFalse(self.sale_journal.check_chronology)
 
-        with common.Form(self.sale_journal) as form:
+        with Form(self.sale_journal) as form:
             form.type = "sale"
         self.assertFalse(self.sale_journal.check_chronology)
 
-        with common.Form(self.sale_journal) as form:
+        with Form(self.sale_journal) as form:
             form.check_chronology = True
         self.assertTrue(self.sale_journal.check_chronology)
 
@@ -105,7 +104,6 @@ class TestAccountInvoiceConstraintChronology(common.TransactionCase):
                 {
                     "date": self.today,
                     "reason": "no reason",
-                    "refund_method": "refund",
                     "journal_id": self.invoice_1.journal_id.id,
                 }
             )
@@ -127,7 +125,6 @@ class TestAccountInvoiceConstraintChronology(common.TransactionCase):
                 {
                     "date": self.today,
                     "reason": "no reason",
-                    "refund_method": "refund",
                     "journal_id": self.invoice_1.journal_id.id,
                 }
             )
@@ -165,93 +162,3 @@ class TestAccountInvoiceConstraintChronology(common.TransactionCase):
         self.invoice_1_5.invoice_date = self.yesterday
         with self.assertRaises(UserError):
             self.invoice_1_5.action_post()
-
-    def test_modify_invoice_date_validated_past_invoice(self):
-        # INV5 YYYYMM20 posted
-        # INV4 YYYYMM15 posted
-        # INV3 YYYYMM10 posted
-        # INV2 YYYYMM05 posted
-        # INV1 YYYYMM01 posted
-        # if we pass INV3 to draft and change the date to YYYYYMM15 or YYYYYMM05
-        # it should be able to validate, but if we change the date
-        # higher than YYYYYMM15 or lower than YYYYYMM05
-        # it should not be able to validate.
-
-        # FIX conflict between 'freeze_time' and account module demo data
-        conflicting_invoices = self.env["account.move"]
-        conflicting_names = [
-            "demo_invoice_1",
-            "demo_invoice_2",
-            "demo_invoice_3",
-            "demo_invoice_followup",
-            "demo_invoice_5",
-        ]
-        for name in conflicting_names:
-            conflicting_invoices += self.env.ref(
-                f"account.{self.env.company.id}_{name}"
-            )
-        conflicting_invoices.button_draft()
-        conflicting_invoices.write(
-            {"name": False, "invoice_date": date.today().replace(day=1)}
-        )
-
-        after_5_days = self.today + timedelta(days=5)
-        after_10_days = self.today + timedelta(days=10)
-        after_15_days = self.today + timedelta(days=15)
-        after_20_days = self.today + timedelta(days=20)
-        after_25_days = self.today + timedelta(days=25)
-
-        self.invoice_1.action_post()
-
-        self.invoice_1_a_5 = self.invoice_1.copy()
-        self.invoice_1_a_5.invoice_date = after_5_days
-        self.invoice_1_a_5.action_post()
-        self.invoice_1_a_10 = self.invoice_1.copy()
-        self.invoice_1_a_10.invoice_date = after_10_days
-        self.invoice_1_a_10.action_post()
-        self.invoice_1_a_15 = self.invoice_1.copy()
-        self.invoice_1_a_15.invoice_date = after_15_days
-        self.invoice_1_a_15.action_post()
-        self.invoice_1_a_20 = self.invoice_1.copy()
-        self.invoice_1_a_20.invoice_date = after_20_days
-        self.invoice_1_a_20.action_post()
-        self.invoice_1_a_25 = self.invoice_1.copy()
-        self.invoice_1_a_25.invoice_date = after_25_days
-        self.invoice_1_a_25.action_post()
-
-        self.invoice_1_a_15.button_cancel()
-        self.invoice_1_a_15.button_draft()
-        self.invoice_1_a_15.invoice_date = after_10_days
-        self.invoice_1_a_15.action_post()
-
-        self.invoice_1_a_15.button_cancel()
-        self.invoice_1_a_15.button_draft()
-        self.invoice_1_a_15.invoice_date = after_10_days - timedelta(days=1)
-        with self.assertRaisesRegex(
-            UserError,
-            f"Chronology conflict: Invoice {self.invoice_1_a_15.name} cannot be before invoice {self.invoice_1_a_10.name}.",
-        ):
-            self.invoice_1_a_15.action_post()
-
-        self.invoice_1_a_15.button_cancel()
-        self.invoice_1_a_15.button_draft()
-        self.invoice_1_a_15.invoice_date = after_20_days
-        self.invoice_1_a_15.action_post()
-
-        self.invoice_1_a_15.button_cancel()
-        self.invoice_1_a_15.button_draft()
-        self.invoice_1_a_15.invoice_date = after_20_days + timedelta(days=1)
-        with self.assertRaisesRegex(
-            UserError,
-            f"Chronology conflict: Invoice {self.invoice_1_a_15.name} cannot be after invoice {self.invoice_1_a_20.name}.",
-        ):
-            self.invoice_1_a_15.action_post()
-
-    def test_post_invoice_with_name(self):
-        # invoice 1 has a number (not /)
-        assert self.invoice_1.state == "draft"
-        assert self.invoice_1.name > "/"
-        # invoice 2 is dated after invoice 1 and is named '/'
-        invoice2 = self.invoice_1.copy()
-        invoice2.invoice_date = self.invoice_1.invoice_date + timedelta(days=1)
-        self.invoice_1.action_post()
